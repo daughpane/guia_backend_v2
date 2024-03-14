dd26520b980938973b3b04bdbcfe49206e8d2fad
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2024-03-14 11:00:59 +0800

    Revert "Revert "more stuff""
    This reverts commit f744bb70220eb83ee4c7de8cdd2c029e94463ef8.

---
+++ middlewares/API.js
@@ -0,0 +1,16 @@
+module.exports = {
+  checkAPI: (req, res, next) => {
+    const apiKey = req.headers["x-api-key"]; // Assuming the API key is passed in the "x-api-key" header
+
+    // Check if the API key is valid
+    if (apiKey === process.env.API_KEY) {
+      // API key is valid
+      next();
+    } else {
+      // API key is invalid
+      return res.status(401).json({
+        error: "Invalid API key",
+      });
+    }
+  },
+};
