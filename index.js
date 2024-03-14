5919b33b8526295fd71b998075de3ee325ecdb46
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2024-03-14 11:34:22 +0800

    update get artworl

--- index.js
+++ index.js
@@ -31,25 +31,43 @@ function getPresignedUrls(key, expiresInSeconds = 86400) {
 app.get("/artworks",checkAPI, async (req, res) => {
   try {
     const client = await pool.connect();
-    console.log(client)
+    const adminId = req.query.admin_id
+    console.log(req.params)
     const result = await client.query(`
-    SELECT *
-    FROM guia_db_artwork AS a
-    JOIN guia_db_section AS s ON a.section_id_id = s.section_id
-    JOIN guia_db_museum AS m ON s.museum_id_id = m.museum_id
-    LEFT JOIN guia_db_artworkimage AS ai ON a.art_id = ai.artwork_id
-    WHERE m.museum_id = 1
-    AND a.is_deleted = FALSE
-    AND ai.is_thumbnail = TRUE
-    ORDER BY a.art_id;
-    `);
+      SELECT 
+        CAST(a.art_id AS INTEGER),
+        a.title, 
+        a.artist_name, 
+        a.medium, 
+        a.date_published, 
+        a.dimen_width_cm, 
+        a.dimen_length_cm, 
+        a.dimen_height_cm, 
+        a.description, 
+        a.additional_info, 
+        a.added_on, 
+        a.updated_on, 
+        a.is_deleted, 
+        s.section_id, 
+        a.added_by_id as added_by, 
+        a.updated_by_id as updated_by, 
+        ai.image_link as image_thumbnail
+      FROM guia_db_artwork AS a
+      JOIN guia_db_section AS s ON a.section_id_id = s.section_id
+      LEFT JOIN guia_db_admin AS ad ON ad.museum_id_id = s.museum_id_id
+      LEFT JOIN guia_db_artworkimage AS ai ON a.art_id = ai.artwork_id
+      WHERE ad.user_id = $1
+      AND a.is_deleted = FALSE
+      AND ai.is_thumbnail = TRUE
+      ORDER BY a.art_id;
+    `,[adminId]);
 
     const artworks = await Promise.all(result.rows.map(async (row) => {
-      const presignedUrl = await getPresignedUrls(row.image_link);
-      return { ...row, image_link: presignedUrl };
+      const presignedUrl = await getPresignedUrls(row.image_thumbnail);
+      return { ...row, image_thumbnail: presignedUrl };
     }));
     
-    res.send(artworks);
+    res.send({artworks: artworks});
     client.release();
   } catch (err) {
     console.error('Error executing query', err);
