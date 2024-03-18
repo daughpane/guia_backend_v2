fe7e6692b85bea8cc557d03e503e889712d7302d
Daughpane Reponte <daughpanereponte@gmail.com>
2024-03-18 23:25:15 +0800

    removed trailing backslash

--- router.js
+++ router.js
@@ -6,8 +6,8 @@ const artworkRouter = require("./api/artwork/artwork.router")
 const adminRouter = require("./api/admin/admin.router")
 const museumRouter = require("./api/museum/museum.router")
 
-router.use("/artwork/", artworkRouter)
-router.use("/admin/", adminRouter)
-router.use("/museum/", museumRouter)    
+router.use("/artwork", artworkRouter)
+router.use("/admin", adminRouter)
+router.use("/museum", museumRouter)    
 
 module.exports = router;
