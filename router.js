4a7666d9aeb26152c9573d294084b9a04724a8df
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2024-03-18 22:39:06 +0800

    necessary changes

--- router.js
+++ router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const artworkRouter = require("./api/artwork/artwork.router")
 const adminRouter = require("./api/admin/admin.router")
 
-router.use("/artwork/", artworkRouter)
-router.use("/admin/", adminRouter)
+router.use("/artwork", artworkRouter)
+router.use("/admin", adminRouter)
 
 module.exports = router;
