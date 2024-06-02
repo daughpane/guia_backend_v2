06589e46153e7cda1297fb11f53fcbd1e5a1c684
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2024-06-03 07:40:08 +0800

    separate S3 config file

--- utils/amazon.js
+++ utils/amazon.js
@@ -1,11 +1,6 @@
-const AWS = require('aws-sdk');
+const { s3 } = require("../config/amazon")
 
 const getPresignedUrls = (key, expiresInSeconds = 86400) => {
-  const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  });
-
   const params = {
     Bucket: process.env.AWS_STORAGE_BUCKET_NAME,
     Key: key,
@@ -16,10 +11,6 @@ const getPresignedUrls = (key, expiresInSeconds = 86400) => {
 };
 
 const getS3Credentials = (image_name, expiresInSeconds = 86400) => {
-  const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  });
 
   const params = {
     Bucket: process.env.AWS_STORAGE_BUCKET_NAME,
